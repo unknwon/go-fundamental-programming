3ce647f3af9d31d77b3788e88dbcf262c4caf3c9
Unknown <joe2010xtmf@163.com>
2013-03-26 16:04:45 -0400

    lecture3

--- lecture3/temp.go
+++ lecture3/temp.go
@@ -4,7 +4,16 @@ import (
 	"fmt"
 )
 
+var (
+	// 使用常规方式
+	aaa = "hello"
+	// 使用并行方式以及类型推断
+	sss, bbb = 1, 2
+	// ccc := 3	// 不可以省略 var
+)
+
 func main() {
-	var a chan
-	fmt.Println(a)
+	var a int = 65
+	b := string(a)
+	fmt.Println(b)
 }
